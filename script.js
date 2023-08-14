9849e9b8297065e39a3e7a7ae68125d57dbba2d2
Trilok P Sharma <34375658+trilok15@users.noreply.github.com>
2023-08-14 14:44:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const scContainer = document.getElementById('js--sc--container');
+
+const sc = new ScratchCard('#js--sc--container', {
+  scratchType: SCRATCH_TYPE.CIRCLE,
+  containerWidth: scContainer.offsetWidth,
+  containerHeight: 75,
+  imageForwardSrc: '/front-image1.png',
+  imageBackgroundSrc: '/scratch-image1.jpeg',
+  // htmlBackground: '<div class="inner_html"><p>Subscribe for more Videos</p></div>',
+  clearZoneRadius: 15,
+  nPoints:30,
+  pointSize: 2,
+})
+
+sc.init().then(() => {
+  sc.canvas.addEventListener('scratch.move', () => {
+    let percent = sc.getPercent().toFixed(0);
+    scInfos.innerHTML = percent + '%';
+    console.log(percent)
+  })
+}).catch((error) => {
+  alert(error.message);
+});
